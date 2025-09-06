d8c461fdbc99fed73f58d5c5657595280b67f687
dhanJi_exp <kdhanji183@gmail.com>
2025-09-06 16:25:30 +0530

    initial commit --d

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-neutral-900">
+      <h1 className="text-white text-5xl">Hello, DevOps Lab!</h1>
+    </div>
+  )
+}
