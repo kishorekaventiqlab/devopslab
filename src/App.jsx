cf3a54afec0d16a54b02b53532bd2bf0b563c8dc
Avinash Prasad <avinashprasad4608@gmail.com>
2025-09-06 16:22:49 +0530

    update code

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+export default function App() {
+  return (
+    <div className="flex bg-neutral-950 justify-center items-center h-screen w-full">
+      <h1 className="text-6xl font-bold text-neutral-50">
+        Hello Devops
+      </h1>
+    </div>
+  )
+}
